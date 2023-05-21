527e30c42847540709f27cd49d159c66120f6eb0
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-05-21 23:02:57 +0530

    making changes

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-#### readind from camera ###############################################
-
-# import cv2 
-# import os
-# # import numpy
-
-# image_path = r'/home/deepjyotisarmah/Documents/Drone_stuffs/images'
-# os.chdir(image_path)
-# cam=cv2.VideoCapture(0)
-# detector=cv2.CascadeClassifier('/home/deepjyotisarmah/Documents/Drone_stuffs/haarcascade_frontalface_default.xml')
-# Id = int(input("enter your id: "))
-# sampleNum = 0
-# while(True):
-#     ret,img=cam.read()
-#     if ret:
-#         gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#         faces=detector.detectMultiScale(gray,1.3,5)
-
-#         for(x,y,w,h) in faces:
-#             cv2.rectangle(img,(x,y),(x+y,w+h),(255,0,0),2)
-#             sampleNum += 1
-#             cv2.imwrite(str(sampleNum)+'.'+"jpg",gray[y:y+h,x:x+w])
-#             cv2.imshow('frame',img)
-#         if sampleNum == Id:
-#             break
-# cam.release()
-# cv2.destroyAllWindows()
-
 ##### reading from a folder ###################################################
 
 import cv2
