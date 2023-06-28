608bc04c961d7daa1b7fc7e8ff0d3c64004000ee
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-28 16:54:21 +0530

    adding images and generating encodings

--- Hog/Youtube/main.py
+++ Hog/Youtube/main.py
@@ -33,14 +33,14 @@ cap.set(4, 480)
 #     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
 # # print(len(imgModeList))
 
-# # Load the encoding file
-# print("Loading Encode File ...")
-# file = open('EncodeFile.p', 'rb')
-# encodeListKnownWithIds = pickle.load(file)
-# file.close()
-# encodeListKnown, studentIds = encodeListKnownWithIds
-# # print(studentIds)
-# print("Encode File Loaded")
+# Load the encoding file
+print("Loading Encode File ...")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded")
 
 # modeType = 0
 # counter = 0
@@ -50,26 +50,26 @@ cap.set(4, 480)
 while True:
     success, img = cap.read()
 
-    # imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    # imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    # faceCurFrame = face_recognition.face_locations(imgS)
-    # encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
     # imgBackground[162:162 + 480, 55:55 + 640] = img
-    # imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+    # imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[1]
 
     # if faceCurFrame:
-    #     for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
-    #         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-    #         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-    #         # print("matches", matches)
-    #         # print("faceDis", faceDis)
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            print("matches", matches)
+            print("faceDis", faceDis)
 
-    #         matchIndex = np.argmin(faceDis)
-    #         # print("Match Index", matchIndex)
+            matchIndex = np.argmin(faceDis)
+            print("Match Index", matchIndex)
 
-    #         if matches[matchIndex]:
+            # if matches[matchIndex]:
     #             # print("Known Face Detected")
     #             # print(studentIds[matchIndex])
     #             y1, x2, y2, x1 = faceLoc
@@ -147,6 +147,6 @@ while True:
     # else:
     #     modeType = 0
     #     counter = 0
-    cv2.imshow("Webcam", img)
+    cv2.imshow("Webcam", img) 
     # cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
